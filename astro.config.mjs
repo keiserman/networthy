cad52035b5d477bd245bdf0b6647b4601fc48f75
Kevin Eiserman <eisermanka@gmail.com>
2024-09-09 19:02:29 -0400

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: "https://keiserman.github.io",
-  base: "networthy",
+  output: "server",
+  adapter: netlify(),
 });
