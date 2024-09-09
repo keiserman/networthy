6a3e76800820e70e984d0bfe5932b50e949365fa
Kevin Eiserman <eisermanka@gmail.com>
2024-09-09 13:00:46 -0400

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -6,5 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  site: "https://keiserman.github.io",
+  base: "networthy",
 });
